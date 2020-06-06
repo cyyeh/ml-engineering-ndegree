b131324d646fc98da252c730538035c9e1947476
cyyeh <chihyu.jimmy.yeh@gmail.com>
2020-06-06 23:30:31 +0800

    remove redundant files

--- projects/capstone-project/deployment/dog_breed_classification/deploy.sh
+++
@@ -1,1 +0,0 @@
-gcloud functions deploy classify-dog-breeds --allow-unauthenticated --entry-point=handler --memory=2048MB --runtime=python37 --source=gs://dog-breed-classifier/project.zip --trigger-http
