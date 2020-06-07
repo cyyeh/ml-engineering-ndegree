ef7eeed3e4f517828946e7a459d96e6c07580fee
cyyeh <chihyu.jimmy.yeh@gmail.com>
2020-06-07 17:12:41 +0800

    update cors

--- projects/capstone-project/deployment/dog_breed_classification/main.py
+++ projects/capstone-project/deployment/dog_breed_classification/main.py
@@ -25,7 +25,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=['*'],
     allow_headers=['*'],
 )
