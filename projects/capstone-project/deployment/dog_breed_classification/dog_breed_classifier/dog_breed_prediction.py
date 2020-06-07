300866646254ce7fed7b562a146f6a915a64247f
cyyeh <chihyu.jimmy.yeh@gmail.com>
2020-06-07 09:40:25 +0800

    add base64 image decoding

--- projects/capstone-project/deployment/dog_breed_classification/dog_breed_classifier/dog_breed_prediction.py
+++ projects/capstone-project/deployment/dog_breed_classification/dog_breed_classifier/dog_breed_prediction.py
@@ -3,10 +3,36 @@ import torch.nn as nn
 import torchvision.models as models
 import torchvision.transforms as transforms
 import numpy as np
-from . import ResNetV2
-
 from PIL import Image
 from PIL import ImageFile
+
+from . import ResNetV2
+
+CLASS_NAMES = [
+    'Affenpinscher', 'Afghan hound', 'Airedale terrier', 'Akita', 'Alaskan malamute', 'American eskimo dog',
+    'American foxhound', 'American staffordshire terrier', 'American water spaniel', 'Anatolian shepherd dog',
+    'Australian cattle dog', 'Australian shepherd', 'Australian terrier', 'Basenji', 'Basset hound', 'Beagle',
+    'Bearded collie', 'Beauceron', 'Bedlington terrier', 'Belgian malinois', 'Belgian sheepdog', 'Belgian tervuren',
+    'Bernese mountain dog', 'Bichon frise', 'Black and tan coonhound', 'Black russian terrier', 'Bloodhound',
+    'Bluetick coonhound', 'Border collie', 'Border terrier', 'Borzoi', 'Boston terrier', 'Bouvier des flandres',
+    'Boxer', 'Boykin spaniel', 'Briard', 'Brittany', 'Brussels griffon', 'Bull terrier', 'Bulldog', 'Bullmastiff',
+    'Cairn terrier', 'Canaan dog', 'Cane corso', 'Cardigan welsh corgi', 'Cavalier king charles spaniel',
+    'Chesapeake bay retriever', 'Chihuahua', 'Chinese crested', 'Chinese shar-pei', 'Chow chow', 'Clumber spaniel',
+    'Cocker spaniel', 'Collie', 'Curly-coated retriever', 'Dachshund', 'Dalmatian', 'Dandie dinmont terrier',
+    'Doberman pinscher', 'Dogue de bordeaux', 'English cocker spaniel', 'English setter', 'English springer spaniel',
+    'English toy spaniel', 'Entlebucher mountain dog', 'Field spaniel', 'Finnish spitz', 'Flat-coated retriever',
+    'French bulldog', 'German pinscher', 'German shepherd dog', 'German shorthaired pointer',
+    'German wirehaired pointer', 'Giant schnauzer', 'Glen of imaal terrier', 'Golden retriever', 'Gordon setter',
+    'Great dane', 'Great pyrenees', 'Greater swiss mountain dog', 'Greyhound', 'Havanese', 'Ibizan hound',
+    'Icelandic sheepdog', 'Irish red and white setter', 'Irish setter', 'Irish terrier', 'Irish water spaniel',
+    'Irish wolfhound', 'Italian greyhound', 'Japanese chin', 'Keeshond', 'Kerry blue terrier', 'Komondor', 'Kuvasz',
+    'Labrador retriever', 'Lakeland terrier', 'Leonberger', 'Lhasa apso', 'Lowchen', 'Maltese', 'Manchester terrier',
+    'Mastiff', 'Miniature schnauzer', 'Neapolitan mastiff', 'Newfoundland', 'Norfolk terrier', 'Norwegian buhund',
+    'Norwegian elkhound', 'Norwegian lundehund', 'Norwich terrier', 'Nova scotia duck tolling retriever',
+    'Old english sheepdog', 'Otterhound', 'Papillon', 'Parson russell terrier', 'Pekingese', 'Pembroke welsh corgi',
+    'Petit basset griffon vendeen', 'Pharaoh hound', 'Plott', 'Pointer', 'Pomeranian', 'Poodle', 'Portuguese water dog',
+    'Saint bernard', 'Silky terrier', 'Smooth fox terrier', 'Tibetan mastiff', 'Welsh springer spaniel',
+    'Wirehaired pointing griffon', 'Xoloitzcuintli', 'Yorkshire terrier']
 
 
 class DogBreedPrediction(object):
@@ -30,25 +56,7 @@ class DogBreedPrediction(object):
                 std=[0.229, 0.224, 0.225],
             ),
         ])
-        self._class_names = ['Affenpinscher', 'Afghan hound', 'Airedale terrier', 'Akita', 'Alaskan malamute', 'American eskimo dog',
-                             'American foxhound', 'American staffordshire terrier', 'American water spaniel', 'Anatolian shepherd dog', 'Australian cattle dog',
-                             'Australian shepherd', 'Australian terrier', 'Basenji', 'Basset hound', 'Beagle', 'Bearded collie', 'Beauceron', 'Bedlington terrier',
-                             'Belgian malinois', 'Belgian sheepdog', 'Belgian tervuren', 'Bernese mountain dog', 'Bichon frise', 'Black and tan coonhound',
-                             'Black russian terrier', 'Bloodhound', 'Bluetick coonhound', 'Border collie', 'Border terrier', 'Borzoi', 'Boston terrier',
-                             'Bouvier des flandres', 'Boxer', 'Boykin spaniel', 'Briard', 'Brittany', 'Brussels griffon', 'Bull terrier', 'Bulldog', 'Bullmastiff',
-                             'Cairn terrier', 'Canaan dog', 'Cane corso', 'Cardigan welsh corgi', 'Cavalier king charles spaniel', 'Chesapeake bay retriever', 'Chihuahua',
-                             'Chinese crested', 'Chinese shar-pei', 'Chow chow', 'Clumber spaniel', 'Cocker spaniel', 'Collie', 'Curly-coated retriever', 'Dachshund',
-                             'Dalmatian', 'Dandie dinmont terrier', 'Doberman pinscher', 'Dogue de bordeaux', 'English cocker spaniel', 'English setter', 'English springer spaniel',
-                             'English toy spaniel', 'Entlebucher mountain dog', 'Field spaniel', 'Finnish spitz', 'Flat-coated retriever', 'French bulldog',
-                             'German pinscher', 'German shepherd dog', 'German shorthaired pointer', 'German wirehaired pointer', 'Giant schnauzer', 'Glen of imaal terrier',
-                             'Golden retriever', 'Gordon setter', 'Great dane', 'Great pyrenees', 'Greater swiss mountain dog', 'Greyhound', 'Havanese', 'Ibizan hound',
-                             'Icelandic sheepdog', 'Irish red and white setter', 'Irish setter', 'Irish terrier', 'Irish water spaniel', 'Irish wolfhound', 'Italian greyhound',
-                             'Japanese chin', 'Keeshond', 'Kerry blue terrier', 'Komondor', 'Kuvasz', 'Labrador retriever', 'Lakeland terrier', 'Leonberger',
-                             'Lhasa apso', 'Lowchen', 'Maltese', 'Manchester terrier', 'Mastiff', 'Miniature schnauzer', 'Neapolitan mastiff', 'Newfoundland',
-                             'Norfolk terrier', 'Norwegian buhund', 'Norwegian elkhound', 'Norwegian lundehund', 'Norwich terrier', 'Nova scotia duck tolling retriever',
-                             'Old english sheepdog', 'Otterhound', 'Papillon', 'Parson russell terrier', 'Pekingese', 'Pembroke welsh corgi', 'Petit basset griffon vendeen',
-                             'Pharaoh hound', 'Plott', 'Pointer', 'Pomeranian', 'Poodle', 'Portuguese water dog', 'Saint bernard', 'Silky terrier', 'Smooth fox terrier',
-                             'Tibetan mastiff', 'Welsh springer spaniel', 'Wirehaired pointing griffon', 'Xoloitzcuintli', 'Yorkshire terrier']
+        self._class_names = CLASS_NAMES
 
     def _detect_dog(self, input_batch):
         with torch.no_grad():
--- projects/capstone-project/deployment/dog_breed_classification/main.py
+++ projects/capstone-project/deployment/dog_breed_classification/main.py
@@ -1,9 +1,19 @@
+import re
+import os
+import base64
+from io import BytesIO
+
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-import os
+from pydantic import BaseModel
+from google.cloud import logging
+
 from dog_breed_classifier import DogBreedPrediction
 from utility import download_blob
+
+logging_client = logging.Client()
+logger = logging_client.logger("dog-breed-classifier")
 
 # keep model as global variable so we don't have to reload
 # it in case of warm invocations
@@ -12,27 +22,35 @@ model = None
 app = FastAPI()
 
 
+class ImageData(BaseModel):
+    base64: str
+
+
 @app.get('/healthcheck')
 def read_healthcheck():
     return 'healthy'
 
 
 @app.post('/classify-dog-breeds')
-def classify_dog_breeds(img_path: str):
+def classify_dog_breeds(img_data: ImageData):
     global model
     BUCKET_NAME = os.getenv('BUCKET_NAME', 'dog-breed-classifier')
     SOURCE_BLOB_NAME = os.getenv('SOURCE_BLOB_NAME', 'model_transfer.pt')
     DESTINATION_FILE_NAME = f'/tmp/{SOURCE_BLOB_NAME}'
 
     if model is None:
+        logger.log_text(f'Initial startup, model content: {model}')
         download_blob(BUCKET_NAME, SOURCE_BLOB_NAME, DESTINATION_FILE_NAME)
         model = DogBreedPrediction(DESTINATION_FILE_NAME)
 
-    '''
-    # testing
-    download_blob(BUCKET_NAME, 'testing/Brittany_02625.jpg',
-                  'tmp/Brittany_02625.jpg')
-    download_blob(BUCKET_NAME, 'testing/sky.jpeg', 'tmp/sky.jpeg')
-
-    return model.predict('tmp/Brittany_02625.jpg')
-    '''
+    # check input data is really an image
+    BASE64_IMAGE_PATTERN = '^data:image/.+;base64,'
+    if re.match(BASE64_IMAGE_PATTERN, img_data.base64):
+        img_data = re.sub(BASE64_IMAGE_PATTERN, '', img_data.base64)
+        img_path = BytesIO(base64.b64decode(img_data))
+        return model.predict(img_path)
+    else:
+        return {
+            'dog_detected': False,
+            'message': 'No image file found, please check again.'
+        }
